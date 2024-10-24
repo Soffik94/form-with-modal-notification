46c7b20492efc0d70e4c8e3d07f7cfad66223f9b
MatejSoffr <matejsoffr94@gmail.com>
2024-10-24 08:06:19 +0200

    code refactoring with useReducer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import Modal from "./components/Modal";
 import data from "./data";
-import { useState } from "react";
+import { useState, useReducer } from "react";
+
+const reducer = () => {};
+
+const defaultState = {
+  albums: [],
+  showNotification: false,
+  notificationContent: "",
+};
 
 const App = () => {
+  const [albumName, setAlbumName] = useState("");
+  const [state, dispatch] = useReducer(reducer, defaultState); //state se pojí s defaultState a reducer s dispatch
+
   const submitHandler = (event) => {
     event.preventDefault();
+
+    if (albumName) {
+    } else {
+    }
   };
 
   return (
     <section>
+      {state.showNotification && <Modal />}
       <form onSubmit={submitHandler}>
-        <input type="text" />
+        <input
+          type="text"
+          value={albumName}
+          onChange={(event) => {
+            setAlbumName(event.target.value);
+          }}
+        />
         <input type="submit" value="přidat" />
       </form>
+      {state.albums.map((oneAlbum) => {
+        const { id, name } = oneAlbum;
+        return <div key={oneAlbum.id}>{name}</div>;
+      })}
     </section>
   );
 };
